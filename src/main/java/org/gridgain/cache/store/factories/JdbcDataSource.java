fd3629eabdb4964a0c76f15e746173946c680a3d
Alexey Kukushkin <AlexeyKukushkin@yahoo.com>
2018-09-04 01:22:12 +0300

    Most popular DB connection properties added to JdbcDataSourceFactory and JdbcDataSource

--- src/main/java/org/gridgain/cache/store/factories/JdbcDataSource.java
+++ src/main/java/org/gridgain/cache/store/factories/JdbcDataSource.java
@@ -26,10 +26,22 @@ import java.util.logging.Logger;
  */
 public class JdbcDataSource implements DataSource {
     /** Database connection URL. */
-    private final String url;
+    private String url;
 
     /** Database connection properties. */
-    private final Map<Object, Object> props;
+    private Map<Object, Object> props;
+
+    /** Database user name. */
+    private String username;
+
+    /** Database user password. */
+    private String pwd;
+
+    /** Database catalog. */
+    private String catalog;
+
+    /** Database schema. */
+    private String schema;
 
     /** Login timeout. */
     private int loginTimeout = 0;
@@ -38,57 +50,29 @@ public class JdbcDataSource implements DataSource {
     private transient PrintWriter logWriter = null;
 
     /** Constructor. */
+    public JdbcDataSource() {
+    }
+
+    /** Constructor. */
     public JdbcDataSource(String url, Map<Object, Object> props) {
         this.url = Objects.requireNonNull(url);
         this.props = props;
     }
 
-    /** {@inheritDoc} */
-    @Override public Connection getConnection() throws SQLException {
-        Connection conn;
-
-        int origLoginTimeout = DriverManager.getLoginTimeout();
-        PrintWriter origLogWriter = DriverManager.getLogWriter();
-        try {
-            if (getLoginTimeout() > 0)
-                DriverManager.setLoginTimeout(getLoginTimeout());
-
-            if (getLogWriter() != null)
-                DriverManager.setLogWriter(getLogWriter());
-
-            if (props == null)
-                conn = DriverManager.getConnection(url);
-            else {
-                Properties p = new Properties();
-                p.putAll(props);
-
-                conn = DriverManager.getConnection(url, p);
-            }
-        }
-        finally {
-            DriverManager.setLoginTimeout(origLoginTimeout);
-            DriverManager.setLogWriter(origLogWriter);
-        }
-
-        return conn;
+    /** Constructor. */
+    public JdbcDataSource(String url, String username, String pwd) {
+        this.url = Objects.requireNonNull(url);
+        this.username = username;
+        this.pwd = pwd;
     }
 
     /** {@inheritDoc} */
-    @Override public Connection getConnection(String user, String pwd) throws SQLException {
-        Properties p = new Properties();
+    @Override public Connection getConnection() throws SQLException {
+        return getConnection(getUsername(), getPassword());
+    }
 
-        if (props != null)
-            p.putAll(props);
-
-        // The implementation assumes user and password connection property names are "user" and "password"
-        // correspondingly. This is just an assumption and not part of the JDBC spec although many JDBC drivers do
-        // use such names.
-        if (user != null)
-            p.setProperty("user", user);
-
-        if (pwd != null)
-            p.setProperty("password", pwd);
-
+    /** {@inheritDoc} */
+    @Override public Connection getConnection(String username, String pwd) throws SQLException {
         int origLoginTimeout = DriverManager.getLoginTimeout();
         PrintWriter origLogWriter = DriverManager.getLogWriter();
         try {
@@ -97,7 +81,27 @@ public class JdbcDataSource implements DataSource {
 
             if (getLogWriter() != null)
                 DriverManager.setLogWriter(getLogWriter());
-            return DriverManager.getConnection(url, p);
+
+            Properties p = new Properties();
+
+            if (username != null)
+                p.setProperty("user", username);
+
+            if (pwd != null)
+                p.setProperty("password", pwd);
+
+            if (getProperties() != null)
+                p.putAll(getProperties());
+
+            Connection conn = DriverManager.getConnection(getUrl(), p);
+
+            if (getCatalog() != null)
+                conn.setCatalog(getCatalog());
+
+            if (getSchema() != null)
+                conn.setSchema(getSchema());
+
+            return conn;
         }
         finally {
             DriverManager.setLoginTimeout(origLoginTimeout);
@@ -142,5 +146,77 @@ public class JdbcDataSource implements DataSource {
     /** {@inheritDoc} */
     @Override public Logger getParentLogger() throws SQLFeatureNotSupportedException {
         throw new SQLFeatureNotSupportedException();
+    }
+
+    /** @return Database connection URL. */
+    public String getUrl() {
+        return url;
+    }
+
+    /** Set database connection URL. */
+    public JdbcDataSource setUrl(String url) {
+        this.url = Objects.requireNonNull(url);
+
+        return this;
+    }
+
+    /** @return Database connection properties. */
+    public Map<Object, Object> getProperties() {
+        return props;
+    }
+
+    /** Set database connection properties. */
+    public JdbcDataSource setProperties(Map<Object, Object> props) {
+        this.props = props;
+
+        return this;
+    }
+
+    /** @return Database catalog. */
+    public String getCatalog() {
+        return catalog;
+    }
+
+    /** Set database catalog. */
+    public JdbcDataSource setCatalog(String catalog) {
+        this.catalog = catalog;
+
+        return this;
+    }
+
+    /** @return Database schema. */
+    public String getSchema() {
+        return schema;
+    }
+
+    /** Set database schema. */
+    public JdbcDataSource setSchema(String schema) {
+        this.schema = schema;
+
+        return this;
+    }
+
+    /** @return Database user name. */
+    public String getUsername() {
+        return username;
+    }
+
+    /** Set database user name. */
+    public JdbcDataSource setUsername(String username) {
+        this.username = username;
+
+        return this;
+    }
+
+    /** @return Database user password. */
+    public String getPassword() {
+        return pwd;
+    }
+
+    /** Set database user password. */
+    public JdbcDataSource setPassword(String pwd) {
+        this.pwd = pwd;
+
+        return this;
     }
 }
